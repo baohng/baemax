83e771f9bd6f035d672eb69cb419fe1706e07df8
vulena <lena09082016@gmail.com>
2022-11-26 16:50:38 +0700

    first commit

---
+++ app/src/main/java/ute/application/baemax/Menu_list.java
@@ -0,0 +1,14 @@
+package ute.application.baemax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_list);
+    }
+}
