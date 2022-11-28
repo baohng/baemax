3cc86f8c4e41a170667ed1d22b1516c8aa36a42c
unknown <ansuu552002@example.com>
2022-11-28 21:15:49 +0700

    change

--- app/src/main/java/ute/application/baemax/MainActivity.java
+++ app/src/main/java/ute/application/baemax/MainActivity.java
@@ -1,61 +1,17 @@
 package ute.application.baemax;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
-import ute.application.baemax.SandwichFragment;
+
 
 
 import android.os.Bundle;
 
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    TabLayout tabLayout;
-     ViewPager2 viewPager;
-     MyViewPagerAdapter myViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
-
-        myViewPagerAdapter = new MyViewPagerAdapter(this);
-        viewPager.setAdapter(myViewPagerAdapter);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-    }
-
-    private void replaceFragment(SandwichFragment fragment){
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.commit();
     }
 }
