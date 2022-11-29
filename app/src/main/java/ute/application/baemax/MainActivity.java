35b91bd30dcc7bcacab42458d4a542a5545d3b81
nhathuy1603 <nhathuy1614@gmail.com>
2022-11-29 13:51:35 +0700

    change file src login

--- app/src/main/java/ute/application/baemax/MainActivity.java
+++ app/src/main/java/ute/application/baemax/MainActivity.java
@@ -2,7 +2,9 @@ package ute.application.baemax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,Register.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
     }
 }
