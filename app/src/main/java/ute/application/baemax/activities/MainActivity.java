d10f854349e96dd772410043787699b5f017b697
Eugen <kirishima219@gmail.com>
2022-12-01 23:24:03 +0700

    change stage

--- app/src/main/java/ute/application/baemax/activities/MainActivity.java
+++ app/src/main/java/ute/application/baemax/activities/MainActivity.java
@@ -2,12 +2,8 @@ package ute.application.baemax.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD:app/src/main/java/ute/application/baemax/activities/MainActivity.java
 
-
-=======
 import android.content.Intent;
->>>>>>> features/na/ui-detail-2:app/src/main/java/ute/application/baemax/MainActivity.java
 import android.os.Bundle;
 
 import ute.application.baemax.R;
