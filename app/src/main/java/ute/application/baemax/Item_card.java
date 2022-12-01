0e16fe9bdf617773ef0ad72d5defe0eb56cb7b16
vulena <lena09082016@gmail.com>
2022-12-01 20:35:04 +0700

    add UI card

---
+++ app/src/main/java/ute/application/baemax/Item_card.java
@@ -0,0 +1,14 @@
+package ute.application.baemax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Item_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_card);
+    }
+}
