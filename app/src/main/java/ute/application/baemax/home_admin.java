beab1074aae1fde2f002f5c95d0c4811a4d1ef93
nhathuy1603 <nhathuy1614@gmail.com>
2022-12-02 09:10:03 +0700

    commit code home admin

--- app/src/main/java/ute/application/baemax/home_admin.java
+++ app/src/main/java/ute/application/baemax/home_admin.java
@@ -2,13 +2,22 @@ package ute.application.baemax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class home_admin extends AppCompatActivity {
-
+    LinearLayout backhere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
+    }
+
+    public void onLoginClick3(View View){
+        startActivity(new Intent(this,LoginAdmin.class));
+        overridePendingTransition(R.anim.slide_in_left,R.anim.stay);
+
     }
 }
