fc577f8222c75418f5993843a88c652348ef1e9e
Eugen <kirishima219@gmail.com>
2022-12-03 22:23:38 +0700

    update file and lib

--- app/src/main/java/ute/application/baemax/activities/DrinksAdapter.java
+++ app/src/main/java/ute/application/baemax/activities/DrinksAdapter.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
