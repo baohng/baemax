b3e9709dca09f5577d51cde058afbf8e3db72904
unknown <ansuu552002@example.com>
2022-12-03 17:33:43 +0700

    bottom navbar

--- app/src/main/java/ute/application/baemax/activities/Profile.java
+++ app/src/main/java/ute/application/baemax/activities/Profile.java
@@ -1,16 +1,55 @@
 package ute.application.baemax.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import ute.application.baemax.R;
+import ute.application.baemax.adapters.ListViewActivity;
+
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        bottomNavigationView.setSelectedItemId(R.id.profileV);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), Homepage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.profileV:
+                        return  true;
+
+                    case R.id.listV:
+                        startActivity(new Intent(getApplicationContext(), ListViewActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.cart:
+                        startActivity(new Intent(getApplicationContext(), CartActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
     }
 }
